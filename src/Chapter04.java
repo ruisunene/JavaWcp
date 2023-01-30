804e8626ae1e96d14dab054119711290e16b13be
堀川龍馬 <horikawaryouma@horikawaryoumanoMacBook-Pro.local>
2023-01-30 16:01:57 +0900

    java

---
+++ src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 8;
+        String str = "龍馬";
+        System.out.println(str);
+        str = "ハルちゃんかわいい";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+	}
+
+}
