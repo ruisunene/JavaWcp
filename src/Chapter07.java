249958fd77288b4b49e8a3b9312739e6b973fe62
堀川龍馬 <horikawaryouma@horikawaryoumanoMacBook-Pro.local>
2023-01-30 16:54:36 +0900

    java

--- src/Chapter07.java
+++ src/Chapter07.java
@@ -1,15 +1,19 @@
 
 public class Chapter07 {
     public static void main(String[] args) {
-        int distance = 12; // 例として12で初期化。他の数値でも良い
-        if (distance <= 5) {
-            System.out.println("とても近いです");
-        } else if (distance <= 10) {
-            System.out.println("近いです");
-         } else if (distance <= 15) {
-            System.out.println("遠いです");
-        } else {
-            System.out.println("とても遠いです");
+        String color = "yellow"; // 例としてyellowで初期化。他の色でも良い
+        switch(color) {
+          case "red" :
+            System.out.println("赤信号です");
+            break;
+          case "yellow" :
+            System.out.println("黄信号です");
+            break;
+          case "blue" :
+            System.out.println("青信号です");
+            break;
+          default:
+            System.out.println("信号の色ではありません");
         }
     }
 }
